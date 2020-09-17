18bf219261f81a1ef40d5a51cb5a2e99a12fa23e
peng <maskjp@tamu.edu>
2020-09-16 23:04:37 -0500

    fix gpu error

--- benchmarks/HRNet-Semantic-Segmentation-HRNet-OCR/run_hrnet.sh
+++ benchmarks/HRNet-Semantic-Segmentation-HRNet-OCR/run_hrnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=/home/usl/Code/Peng/data_collection/benchmarks/HRNet-Semantic-Segmentation-HRNet-OCR/:$PYTHONPATH
 echo $PYTHONPATH
-PY_CMD="python -m torch.distributed.launch --nproc_per_node=4"
+PY_CMD="python -m torch.distributed.launch --nproc_per_node=2"
 python /home/usl/Code/Peng/data_collection/benchmarks/scripts/test.py
 $PY_CMD tools/train.py --cfg experiments/rellis/seg_hrnet_ocr_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484.yaml
