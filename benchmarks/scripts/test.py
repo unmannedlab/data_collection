228b41a51a3f29235a3b3e67d898f8143885b679
peng <maskjp@tamu.edu>
2020-09-16 22:25:02 -0500

    modify path

---
+++ benchmarks/scripts/test.py
@@ -0,0 +1,10 @@
+import os   
+      
+# Get the current working   
+# directory (CWD)   
+cwd = os.getcwd()   
+      
+# Print the current working    
+# directory (CWD)   
+print("Current working directory:") 
+print(cwd)
