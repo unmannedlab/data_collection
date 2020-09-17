eb3bcac8233e358d32c2603789e2828a66166f7f
peng <maskjp@tamu.edu>
2020-09-16 22:05:26 -0500

    add script

---
+++ benchmarks/scripts/run_hrnet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PYTHONPATH=/home/usl/Code/Peng/data_collection/benchmarks/HRNet-Semantic-Segmentation-HRNet-OCR/:$PYTHONPATH
+PY_CMD="python -m torch.distributed.launch --nproc_per_node=4"
+$PY_CMD tools/train.py --cfg experiments/rellis/seg_hrnet_ocr_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484.yaml
