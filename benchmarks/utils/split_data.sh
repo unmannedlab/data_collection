cf18585fc894a1f7453be0858c7c151e618f9285
peng <maskjp@tamu.edu>
2020-09-29 11:18:25 -0500

    modify data split

--- benchmarks/utils/split_data.sh
+++ benchmarks/utils/split_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python split_data.py --root_path /home/usl/Datasets/rellis/ \
-    --sequences 00002_2 00004 \
+    --sequences 00004 \
     --df_name pylon_camera_node \
     --dl_name pylon_camera_node_label_id \
     --data_ext jpg \
