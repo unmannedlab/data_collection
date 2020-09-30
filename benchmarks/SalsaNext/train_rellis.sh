00f422c5144725a8401e7e0a1ce21a77f36de84a
peng <maskjp@tamu.edu>
2020-09-29 21:15:33 -0500

    update shell

--- benchmarks/SalsaNext/train_rellis.sh
+++ benchmarks/SalsaNext/train_rellis.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export CUDA_VISIBLE_DEVICES="$c"
-cd ./train/tasks/semantic;  ./train.py -d rellis  -ac ./salsanext_ouster.yml -dc ./config/labels/rellis.yaml -n rellis
+cd ./train/tasks/semantic;  ./train.py -d rellis  -ac ./config/arch/salsanext_ouster.yml -dc ./config/labels/rellis.yaml -n rellis
